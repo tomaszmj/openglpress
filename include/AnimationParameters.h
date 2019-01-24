e43c55fe058cf6d6bbb057b55dd79c3e25255538
tomaszmj <tomasz.m.j.nowak@gmail.com>
2019-01-24 03:17:53 +0100

    drastically simplified Animation Parameters - only linear equations

--- include/AnimationParameters.h
+++ include/AnimationParameters.h
@@ -3,32 +3,27 @@
 #include <glm/glm.hpp>
 
 // AnimationParamters constructor:
-// 6 times (each in seconds, counted form animation start) are:
+// 4 times (each in seconds, counted form animation start) are:
 //   time when press starts moving down
-//   time when press hits cylinder under the press
 //   time when press stops after crushing cylinder
 //   time when press starts moving up again, together with the crushed cylinder (which is regaining its shape)
-//   time when the cylinder under the press regains its shape, while press continues moving up
 //   time when press comes back to the original position
-// requirement: times[i + 1] > times[i] for each i in {0, 1, 2, 3, 4}
+// additionally, two more times will be computed:
+//   time when press hits cylinder under the press
+//   time when the cylinder under the press regains its shape, while press continues moving up
+// constraint: times[i + 1] > times[i] for each i in {0, 1, 2, 3, 4}
 // 3 heights (y positions in world coordinates):
 //   initial position of press lower base
 //   initial position of upper base of cylinder under the press
 //   final position of press and crushed cylinder
-// requirement: heights[0] > heights[1] > heights[2]
-// The code calculating other parameters based on the input described above may not
-// be readable, because the equations have been derived by hand on a piece of paper.
+// constraint: heights[0] > heights[1] > heights[2]
 struct AnimationParameters
 {
-    AnimationParameters(const std::array<double, 6> times, const std::array<double, 3> heights);
-    const std::array<double, 6> t;
-    const std::array<double, 3> h;
+    AnimationParameters(const std::array<double, 4> times, const std::array<double, 3> heights);
+    std::array<double, 6> t;
+    std::array<double, 3> h;
     double maxVelocityBeforeCrushing;
     double maxVelocityAfterCrushing;
-    double accelerationBeforeCrushing;
-    double accelerationAfterCrushing;
-    std::array<double, 3> quadraticVelocityCoefficientsBeforeCrushing;
-    std::array<double, 3> quadraticVelocityCoefficientsAfterCrushing;
 
     double calculateY(double time) const;
     static std::array<double, 3> solveLinearEquationsSystem(const glm::dmat3 &m, const glm::dvec3 &v);
--- src/AnimationParameters.cpp
+++ src/AnimationParameters.cpp
@@ -3,28 +3,17 @@
 #include <cassert>
 #include <glm/gtc/matrix_access.hpp>
 
-AnimationParameters::AnimationParameters(const std::array<double, 6> times, const std::array<double, 3> heights)
-    : t(times), h(heights)
+AnimationParameters::AnimationParameters(const std::array<double, 4> times, const std::array<double, 3> heights)
+    : h(heights)
 {
-    maxVelocityBeforeCrushing = 2 * (h[0] - h[1]) / (t[1] - t[0]);
-    accelerationBeforeCrushing = maxVelocityBeforeCrushing / (t[1] - t[0]);
-    maxVelocityAfterCrushing = 2 * (h[0] - h[1]) / (t[5] - t[4]);
-    accelerationAfterCrushing = maxVelocityAfterCrushing / (t[5] - t[4]);
-    glm::dmat3 m(
-        glm::dvec3(t[1]*t[1], t[2]*t[2], (std::pow(t[2], 3) - std::pow(t[1], 3))/3.0),
-        glm::dvec3(t[1], t[2], (t[2]*t[2] - t[1]*t[1])/2.0),
-        glm::dvec3(1.0, 1.0, t[2] - t[1])
-    );
-    assert(glm::all(glm::equal(glm::column(m, 0), glm::dvec3(t[1]*t[1], t[2]*t[2], (std::pow(t[2], 3) - std::pow(t[1], 3))/3.0))));
-    glm::dvec3 v(maxVelocityBeforeCrushing, 0.0, h[1] - h[2]);
-    quadraticVelocityCoefficientsBeforeCrushing = solveLinearEquationsSystem(m, v);
-    m = glm::dmat3(
-        glm::dvec3(t[3]*t[3], t[4]*t[4], (std::pow(t[4], 3) - std::pow(t[3], 3))/3.0),
-        glm::dvec3(t[3], t[4], (t[4]*t[4] - t[3]*t[3])/2.0),
-        glm::dvec3(1.0, 1.0, t[4] - t[3])
-    );
-    v = glm::dvec3(0.0, maxVelocityAfterCrushing, h[1] - h[2]);
-    quadraticVelocityCoefficientsAfterCrushing = solveLinearEquationsSystem(m, v);
+    t[0] = times[0];
+    t[2] = times[1];
+    t[3] = times[2];
+    t[5] = times[3];
+    maxVelocityBeforeCrushing = 2 * (h[0] - h[2]) / (t[2] - t[0]);
+    maxVelocityAfterCrushing = 2 * (h[0] - h[2]) / (t[5] - t[3]);
+    t[1] = 2 * (h[0] - h[1]) / maxVelocityBeforeCrushing + t[0];
+    t[4] = 2 * (h[1] - h[2]) / maxVelocityAfterCrushing + t[3];
 }
 
 double AnimationParameters::calculateY(double time) const
@@ -34,29 +23,24 @@ double AnimationParameters::calculateY(double time) const
     if(time < t[0])
         return h[0];
     if(time < t[1])
-        return h[0] - 0.5 * accelerationBeforeCrushing * (time - t[0]) * (time - t[0]);
+    {
+        double v = maxVelocityBeforeCrushing * (time - t[0]) / (t[1] - t[0]);
+        return h[0] - 0.5 * v * (time - t[0]);
+    }
     if(time < t[2])
     {
-        const auto &q = quadraticVelocityCoefficientsBeforeCrushing;
-        double delta_y =
-            q[0] * (std::pow(time, 3) - std::pow(t[1], 3)) / 3.0 +
-            q[1] * (time * time - t[1] * t[1]) / 2.0 +
-            q[2] * (time - t[1]);
-        return h[1] - delta_y;
+        double v = maxVelocityBeforeCrushing * (t[2] - time) / (t[2] - t[1]);
+        return h[1] - 0.5 * (maxVelocityBeforeCrushing + v) * (time - t[1]);
     }
     if(time < t[3])
         return h[2];
     if(time < t[4])
     {
-        const auto &q = quadraticVelocityCoefficientsAfterCrushing;
-        double delta_y =
-            q[0] * (std::pow(time, 3) - std::pow(t[3], 3)) / 3.0 +
-            q[1] * (time * time - t[3] * t[3]) / 2.0 +
-            q[2] * (time - t[3]);
-        return h[2] + delta_y;
+        double v = maxVelocityAfterCrushing * (time - t[3]) / (t[4] - t[3]);
+        return h[2] + 0.5 * v * (time - t[3]);
     }
     // else - if time <= t[5]
-    double v = maxVelocityAfterCrushing - accelerationAfterCrushing * (time - t[4]);
+    double v = maxVelocityAfterCrushing * (t[5] - time) / (t[5] - t[4]);
     return h[1] + 0.5 * (v + maxVelocityAfterCrushing) * (time - t[4]);
 }
 
